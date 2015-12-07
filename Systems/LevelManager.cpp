131c9479bb79cc8f2847b16073ce2936773017f8
0x2c <bakim@ucsd.edu>
2015-12-07 02:09:26 -0800

    Moar treess!!! Fast in only release mode .__.

--- Systems/LevelManager.cpp
+++ Systems/LevelManager.cpp
@@ -34,20 +34,47 @@ void LevelManager::LoadTestLevel() {
 	refSG->addChild(forestMT);
 
 	MatrixTransform *tree0MT = new MatrixTransform();
+	MatrixTransform *tree1MT = new MatrixTransform();
+	MatrixTransform *tree2MT = new MatrixTransform();
+	MatrixTransform *tree3MT = new MatrixTransform();
+	MatrixTransform *tree4MT = new MatrixTransform();
+
 	int height = refSG->terrain->terrainGetHeight(0,0);
 	tree0MT->setMatrix(mat4().makeIdentity().setTranslate(vec3(0.f, (float)(height) - 0.3f, 0.f)));
+	height = refSG->terrain->terrainGetHeight(6,-6);
+	tree1MT->setMatrix(mat4().makeIdentity().setTranslate(vec3(6.f, (float)(height) - 0.3f, -6.f)));
+	height = refSG->terrain->terrainGetHeight(-6,0);
+	tree2MT->setMatrix(mat4().makeIdentity().setTranslate(vec3(-6.f, (float)(height) - 0.3f, 0.f)));
+	height = refSG->terrain->terrainGetHeight(6,0);
+	tree3MT->setMatrix(mat4().makeIdentity().setTranslate(vec3(6.f, (float)(height) - 0.3f, 0.f)));
+	height = refSG->terrain->terrainGetHeight(0,-6);
+	tree4MT->setMatrix(mat4().makeIdentity().setTranslate(vec3(0.f, (float)(height) - 0.3f, -6.f)));
+
 	forestMT->addChild(tree0MT);
+	forestMT->addChild(tree1MT);
+	forestMT->addChild(tree2MT);
+	forestMT->addChild(tree3MT);
+	forestMT->addChild(tree4MT);
 	
 	// Creating our first tree
-	std::string tree0_ID = "Tree::sample0";
+	std::string treeSample = "Tree::sample";
 	
 	EntityNode *tree0_EN = new EntityNode();
-	tree0_EN->name = tree0_ID;
+	tree0_EN->name = treeSample;
 	tree0_EN->parts = TreeGenerator.generate();
 	refES->insert(tree0_EN);
 
-	Tree *sampleTree = new Tree(tree0_ID);
-	tree0MT->addChild(sampleTree);
+	Tree *tree0 = new Tree(treeSample);
+	Tree *tree1 = new Tree(treeSample);
+	Tree *tree2 = new Tree(treeSample);
+	Tree *tree3 = new Tree(treeSample);
+	Tree *tree4 = new Tree(treeSample);
+
+	tree0MT->addChild(tree0);
+	tree1MT->addChild(tree1);
+	tree2MT->addChild(tree2);
+	tree3MT->addChild(tree3);
+	tree4MT->addChild(tree4);
 
 }
 
